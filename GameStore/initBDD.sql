9c12160f0ef17e74137f7e827710db63c29c249e
KarlakS2 <karlak.s@laposte.net>
2014-03-21 17:49:42 +0100

    sql corrigé

--- GameStore/initBDD.sql
+++ GameStore/initBDD.sql
@@ -6,7 +6,7 @@ INSERT  INTO ARTICLE(NOM,PRIX,DESCRIPTION,URLIMAGE,CATEGORIE)
         VALUES ('Metal Gear Solid',
                 42,
                 'Metal Gear Solid est un jeu dinfiltration sorti sur PlayStation. Dans cet opus, Solid Snake doit infiltrer Shadow Moses, une base militaire en Alaska, afin de sauver des otages. Le joueur trouve tout au long de laventure de nouvelles armes et nouveaux équipements tout en alternant dialogues, séquences dinfiltration et combats contre les boss. Le scénario hollywoodien allié à de nombreuses cinématiques en font un jeu très cinématographique.',
-                'image/miniatureArticle/mgs1mini',
+                'image/miniatureArticle/mgs1mini.jpg',
                 'Playsation'
                 );
 
@@ -14,7 +14,7 @@ INSERT  INTO ARTICLE(NOM,PRIX,DESCRIPTION,URLIMAGE,CATEGORIE)
         VALUES ('Metal Gear Solid 2',
                 51,
                 'MGS2 : Sons of Liberty est un jeu dinfiltration/action sorti sur PS2. Dans cet opus le joueur incarne Snake puis Raiden dans une lutte contre la prolifération du Metal Gear. Equipé de divers armes et gadgets, il doit sinfiltrer pour arriver au but, mais libre à lui dutiliser la manière forte. Senchaînent alors phases dinfiltration, daction, de boss. Le scénario dense comte de nombreuses cinématiques pour un jeu très proche du 7ème art.',
-                'image/miniatureArticle/mgs2mini',
+                'image/miniatureArticle/mgs2mini.jpg',
                 'Playsation 2'
                 );
 
@@ -22,7 +22,7 @@ INSERT  INTO ARTICLE(NOM,PRIX,DESCRIPTION,URLIMAGE,CATEGORIE)
         VALUES ('Metal Gear Solid 3',
                 51,
                 'MGS3 sorti sur PS2 se déroule avant les autres épisodes de la saga. Sur fond de guerre froide, le joueur y incarne Naked Snake et doit infiltrer le territoire russe pour rapatrier un scientifique. Explorant la jungle, les montagnes et autres complexes militaires, Snake a à sa disposition tout un panel darmes et de gadgets. Outre ces phases dinfiltration/action, le travail et la présence des scénaristes relèvent des plus grandes montures hollywoodiennes.',
-                'image/miniatureArticle/mgs3mini',
+                'image/miniatureArticle/mgs3mini.jpg',
                 'Playsation 2'
                 );
 
@@ -30,6 +30,6 @@ INSERT  INTO ARTICLE(NOM,PRIX,DESCRIPTION,URLIMAGE,CATEGORIE)
         VALUES ('Metal Gear Solid 4',
                 24,
                 'Quatrième épisode de la saga Metal Gear Solid, Guns of the Patriots reprend sur PS3 les mécanismes des anciens opus en mélangeant action et infiltration. Répondant à toutes les questions laissées en suspens, cet ultime segment fait une fois de plus la part belle aux cinématiques hollywoodiennes mettant en avant un casting haut de gamme.',
-                'image/miniatureArticle/mgs4mini',
+                'image/miniatureArticle/mgs4mini.jpg',
                 'Playsation 3'
                 );
